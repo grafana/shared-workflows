e3d8a0c85ba04666853dbd50d6ef82fec7cd6dfc
Horst Gutmann <horst.gutmann@grafana.com>
2024-04-18 10:00:51 +0200

    WIP: Load docs dir from mkdocs.yml file

--- actions/techdocs-rewrite-relative-links/go.mod
+++ actions/techdocs-rewrite-relative-links/go.mod
@@ -5,11 +5,15 @@ go 1.21.6
 require (
 	github.com/stretchr/testify v1.7.0
 	github.com/teekennedy/goldmark-markdown v0.3.0
+	github.com/urfave/cli/v2 v2.27.1
 	github.com/yuin/goldmark v1.7.1
 )
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- actions/techdocs-rewrite-relative-links/main.go
+++ actions/techdocs-rewrite-relative-links/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"bytes"
 	"context"
-	"flag"
-	"fmt"
 	"io/fs"
 	"log/slog"
 	"os"
@@ -12,33 +10,79 @@ import (
 	"strings"
 
 	markdown "github.com/teekennedy/goldmark-markdown"
+	"github.com/urfave/cli/v2"
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/ast"
 	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/text"
 	"github.com/yuin/goldmark/util"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
-	var repoURL string
-	var defaultBranch string
-	flag.StringVar(&repoURL, "repo-url", "https://github.com/grafana/deployment_tools", "Full URL of the repository on GitHub")
-	flag.StringVar(&defaultBranch, "default-branch", "main", "Name of the default branch")
-	flag.Parse()
+	app := cli.App{
+		Name: "techdocs-rewrite-relative-links",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "root-dir",
+				Usage:    "Path to the directory where the mkdocs.yml file is located",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "repo-url",
+				Usage:    "Full URL of the repository on GitHub",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "default-branch",
+				Usage:    "Name of the default branch",
+				Required: true,
+			},
+		},
+		Action: func(cliCtx *cli.Context) error {
+			repoURL := cliCtx.String("repo-url")
+			defaultBranch := cliCtx.String("default-branch")
+			rootDir := cliCtx.String("root-dir")
 
-	ctx := context.Background()
-	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
-	docsRoot := filepath.Join(flag.Arg(0), "docs")
-	ctrl := controller{
-		logger:            logger,
-		docsRootDirectory: docsRoot,
-		rootDirectory:     flag.Arg(0),
-		repoURL:           repoURL,
-		defaultBranch:     defaultBranch,
+			ctx := cliCtx.Context
+			logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+			ctrl := controller{
+				logger:        logger,
+				rootDirectory: rootDir,
+				repoURL:       repoURL,
+				defaultBranch: defaultBranch,
+			}
+			return ctrl.run(ctx)
+		},
 	}
-	fmt.Println(filepath.WalkDir(docsRoot, func(path string, d fs.DirEntry, err error) error {
+	ctx := context.Background()
+	app.RunContext(ctx, os.Args)
+}
+
+type controller struct {
+	logger        *slog.Logger
+	defaultBranch string
+	repoURL       string
+	rootDirectory string
+
+	// Private: This will be set during the run
+	docsRootDirectory string
+}
+
+func (ctrl *controller) run(ctx context.Context) error {
+	docsRootDirectory, err := ctrl.getDocsDir(ctx, ctrl.rootDirectory)
+	ctrl.docsRootDirectory = docsRootDirectory
+	if err != nil {
+		return err
+	}
+	return filepath.WalkDir(docsRootDirectory, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
+		}
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
 		}
 		if !d.IsDir() && strings.HasSuffix(d.Name(), ".md") {
 			if err := ctrl.updateRelativeLinks(ctx, path); err != nil {
@@ -46,15 +90,7 @@ func main() {
 			}
 		}
 		return nil
-	}))
-}
-
-type controller struct {
-	logger            *slog.Logger
-	defaultBranch     string
-	repoURL           string
-	rootDirectory     string
-	docsRootDirectory string
+	})
 }
 
 func (ctrl *controller) updateRelativeLinks(ctx context.Context, path string) error {
@@ -62,6 +98,7 @@ func (ctrl *controller) updateRelativeLinks(ctx context.Context, path string) er
 	renderer := markdown.NewRenderer()
 	markdown := goldmark.New(goldmark.WithRenderer(renderer))
 	transformer := &relativeLinkASTTransformer{
+		ctx:               ctx,
 		logger:            logger,
 		rootDirectory:     ctrl.rootDirectory,
 		docsRootDirectory: ctrl.docsRootDirectory,
@@ -79,6 +116,7 @@ func (ctrl *controller) updateRelativeLinks(ctx context.Context, path string) er
 		return err
 	}
 	if transformer.changed {
+		// Only update the original file if there were actual changes made:
 		if err := os.WriteFile(path, out.Bytes(), 0600); err != nil {
 			return err
 		}
@@ -86,7 +124,27 @@ func (ctrl *controller) updateRelativeLinks(ctx context.Context, path string) er
 	return nil
 }
 
+func (ctrl *controller) getDocsDir(ctx context.Context, root string) (string, error) {
+	cfg := mkdocsYaml{}
+	content, err := os.ReadFile(filepath.Join(root, "mkdocs.yml"))
+	if err != nil {
+		return "", err
+	}
+	if err := yaml.Unmarshal(content, &cfg); err != nil {
+		return "", err
+	}
+	if filepath.IsAbs(cfg.DocsDir) {
+		return cfg.DocsDir, nil
+	}
+	return filepath.Join(root, cfg.DocsDir), nil
+}
+
+type mkdocsYaml struct {
+	DocsDir string `yaml:"docs_dir"`
+}
+
 type relativeLinkASTTransformer struct {
+	ctx               context.Context
 	logger            *slog.Logger
 	rootDirectory     string
 	docsRootDirectory string
@@ -114,7 +172,7 @@ func (transformer *relativeLinkASTTransformer) Transform(node *ast.Document, rea
 						typeSegment = "blob"
 					}
 					newDest := strings.Replace(absPath, strings.TrimSuffix(transformer.rootDirectory, "/"), transformer.repoURL+"/"+typeSegment+"/"+transformer.defaultBranch, 1)
-					transformer.logger.Info("rewriting path", slog.String("old-dest", dst), slog.String("new-dest", newDest))
+					transformer.logger.InfoContext(transformer.ctx, "rewriting path", slog.String("old-dest", dst), slog.String("new-dest", newDest))
 					// If this is too aggressive, we should be able to
 					// determine the line of the link and then just replace the
 					// string in a separate run
