fc49f8b0b4ceeaf05762176cf80bafd101f7f746
Horst Gutmann <horst.gutmann@grafana.com>
2024-04-22 09:14:41 +0200

    Table tests

--- actions/techdocs-rewrite-relative-links/main_test.go
+++ actions/techdocs-rewrite-relative-links/main_test.go
@@ -12,90 +12,100 @@ import (
 
 func TestUpdateRelativeLinks(t *testing.T) {
 	logger := slogt.New(t)
-	t.Run("rewrites-outside-link", func(t *testing.T) {
-		rootDir := t.TempDir()
-		createTestDirectory(t, rootDir, map[string]string{
-			"README.md":      "outside file",
-			"docs/README.md": "Hello world [inside](./README.md) [outside](../README.md)",
-		})
-		ctrl := controller{
-			rootDirectory:     rootDir,
-			logger:            logger,
-			defaultBranch:     "main",
-			repoURL:           "https://github.com/grafana/dummy",
-			docsRootDirectory: filepath.Join(rootDir, "docs"),
-		}
-		testFilePath := filepath.Join(rootDir, "docs", "README.md")
-		require.NoError(t, ctrl.updateRelativeLinks(context.Background(), filepath.Join(rootDir, "docs/README.md")))
-		updatedContent, err := os.ReadFile(testFilePath)
-		require.NoError(t, err)
-		require.Equal(t, "Hello world [inside](./README.md) [outside](https://github.com/grafana/dummy/blob/main/README.md)\n", string(updatedContent))
-	})
+	tests := map[string]struct {
+		testDirectory       map[string]string
+		fileUnderTest       string
+		expectedFileContent string
+	}{
+		"rewrites-outside-link": {
+			testDirectory: map[string]string{
+				"README.md":      "outside file",
+				"docs/README.md": "Hello world [inside](./README.md) [outside](../README.md)",
+			},
+			fileUnderTest:       "docs/README.md",
+			expectedFileContent: "Hello world [inside](./README.md) [outside](https://github.com/grafana/dummy/blob/main/README.md)\n",
+		},
+		"ignores-external-links": {
+			testDirectory: map[string]string{
+				"docs/README.md": "Hello world [external](https://example.org)",
+			},
+			fileUnderTest:       "docs/README.md",
+			expectedFileContent: "Hello world [external](https://example.org)",
+		},
+	}
 
-	t.Run("ignores-external-links", func(t *testing.T) {
-		rootDir := t.TempDir()
-		createTestDirectory(t, rootDir, map[string]string{
-			"docs/README.md": "Hello world [external](https://example.org)",
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			rootDir := t.TempDir()
+			createTestDirectory(t, rootDir, test.testDirectory)
+			ctrl := controller{
+				rootDirectory:     rootDir,
+				logger:            logger,
+				defaultBranch:     "main",
+				repoURL:           "https://github.com/grafana/dummy",
+				docsRootDirectory: filepath.Join(rootDir, "docs"),
+			}
+			testFilePath := filepath.Join(rootDir, test.fileUnderTest)
+			require.NoError(t, ctrl.updateRelativeLinks(context.Background(), filepath.Join(rootDir, test.fileUnderTest)))
+			updatedContent, err := os.ReadFile(testFilePath)
+			require.NoError(t, err)
+			require.Equal(t, test.expectedFileContent, string(updatedContent))
 		})
-		ctrl := controller{
-			rootDirectory:     rootDir,
-			logger:            logger,
-			defaultBranch:     "main",
-			repoURL:           "https://github.com/grafana/dummy",
-			docsRootDirectory: filepath.Join(rootDir, "docs"),
-		}
-		testFilePath := filepath.Join(rootDir, "docs", "README.md")
-		require.NoError(t, ctrl.updateRelativeLinks(context.Background(), filepath.Join(rootDir, "docs/README.md")))
-		updatedContent, err := os.ReadFile(testFilePath)
-		require.NoError(t, err)
-		require.Equal(t, "Hello world [external](https://example.org)", string(updatedContent))
-	})
+	}
 }
 
 func TestGetDocsRoot(t *testing.T) {
 	logger := slogt.New(t)
-	t.Run("no-mkdocs-yml", func(t *testing.T) {
-		rootDir := t.TempDir()
-		createTestDirectory(t, rootDir, map[string]string{})
-		ctrl := controller{
-			rootDirectory: rootDir,
-			logger:        logger,
-			defaultBranch: "main",
-			repoURL:       "https://github.com/grafana/dummy",
-		}
-		_, err := ctrl.getDocsDir(context.Background(), rootDir)
-		require.Error(t, err)
-	})
-	t.Run("mkdocs-yml-relative-docs-dir", func(t *testing.T) {
-		rootDir := t.TempDir()
-		createTestDirectory(t, rootDir, map[string]string{
-			"mkdocs.yml": `docs_dir: "docs"`,
+	tests := map[string]struct {
+		expectError             bool
+		expectedRelativeDocsDir string
+		expectedAbsoluteDocsDir string
+		rootDirectory           map[string]string
+	}{
+		"no-mkdocs-yml": {
+			expectError:   true,
+			rootDirectory: map[string]string{},
+		},
+		"mkdocs-yml-relative-docs-dir": {
+			expectError: false,
+			rootDirectory: map[string]string{
+				"mkdocs.yml": `docs_dir: "docs"`,
+			},
+			expectedRelativeDocsDir: "docs",
+		},
+		"mkdocs-yml-absolute-docs-dir": {
+			expectError: false,
+			rootDirectory: map[string]string{
+				"mkdocs.yml": `docs_dir: "/tmp/docs"`,
+			},
+			expectedAbsoluteDocsDir: "/tmp/docs",
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			rootDir := t.TempDir()
+			createTestDirectory(t, rootDir, test.rootDirectory)
+			ctrl := controller{
+				rootDirectory: rootDir,
+				logger:        logger,
+				defaultBranch: "main",
+				repoURL:       "https://github.com/grafana/dummy",
+			}
+			docsDir, err := ctrl.getDocsDir(context.Background(), rootDir)
+			if test.expectError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+			if test.expectedAbsoluteDocsDir != "" {
+				require.Equal(t, test.expectedAbsoluteDocsDir, docsDir)
+			}
+			if test.expectedRelativeDocsDir != "" {
+				require.Equal(t, filepath.Join(rootDir, test.expectedRelativeDocsDir), docsDir)
+			}
 		})
-		ctrl := controller{
-			rootDirectory: rootDir,
-			logger:        logger,
-			defaultBranch: "main",
-			repoURL:       "https://github.com/grafana/dummy",
-		}
-		docsDir, err := ctrl.getDocsDir(context.Background(), rootDir)
-		require.NoError(t, err)
-		require.Equal(t, filepath.Join(rootDir, "docs"), docsDir)
-	})
-	t.Run("mkdocs-yml-absolute-docs-dir", func(t *testing.T) {
-		rootDir := t.TempDir()
-		createTestDirectory(t, rootDir, map[string]string{
-			"mkdocs.yml": `docs_dir: "/tmp/docs"`,
-		})
-		ctrl := controller{
-			rootDirectory: rootDir,
-			logger:        logger,
-			defaultBranch: "main",
-			repoURL:       "https://github.com/grafana/dummy",
-		}
-		docsDir, err := ctrl.getDocsDir(context.Background(), rootDir)
-		require.NoError(t, err)
-		require.Equal(t, filepath.Join("/tmp", "docs"), docsDir)
-	})
+	}
 }
 
 func createTestDirectory(t *testing.T, rootDir string, content map[string]string) {
