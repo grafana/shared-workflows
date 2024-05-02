93c84040a318ceb535ed130b9b75c76eb68b0a06
Horst Gutmann <horst.gutmann@grafana.com>
2024-05-02 08:17:04 +0100

    feat: Add new techdocs-rewrite-relative-links-action (#99)
    * feat: Add new techdocs-rewrite-relative-links-action
    
    * WIP: Load docs dir from mkdocs.yml file
    
    * Hook action into publish-techdocs workflow
    
    * Disable publishing in order to do testruns
    
    * s/input/inputs/g
    
    * Fix action branch
    
    * Fix action name
    
    * Fix go run call
    
    * Use action_ref to link workflow and action
    
    * Uses does not support expressions, undoing previous commit
    
    * Show diff of changes for review
    
    * Add more tests
    
    * Use local action
    
    * Use workflow-ref
    
    * Fallback for pull requests
    
    * Falling back to non-relative action (lower complexity)
    
    * Cleanup + verbose input flag
    
    * Use slogt for test logging
    
    * Use signal.NotifyContext for the root context
    
    * Table tests
    
    * Use actionslog inside GHA context
    
    * Return early
    
    * Add dry-run mode that only prints a diff
    
    * Enable local actions through customizable checkout path
    
    * Move fs interactions to afero for better mocking
    
    * Add documentation
    
    * Explicitly use Go 1.22.2
    
    * Rename skip-checkout to checkout
    
    * Fix linting issues
    
    * Prepare for merge

---
+++ actions/techdocs-rewrite-relative-links/main.go
@@ -0,0 +1,241 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/fs"
+	"log/slog"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strings"
+	"syscall"
+
+	"github.com/aymanbagabas/go-udiff"
+	"github.com/lmittmann/tint"
+	"github.com/spf13/afero"
+	markdown "github.com/teekennedy/goldmark-markdown"
+	"github.com/urfave/cli/v2"
+	"github.com/willabides/actionslog"
+	"github.com/willabides/actionslog/human"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/ast"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/text"
+	"github.com/yuin/goldmark/util"
+	"golang.org/x/term"
+	"gopkg.in/yaml.v3"
+)
+
+func main() {
+	app := cli.App{
+		Name: "techdocs-rewrite-relative-links",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "root-dir",
+				Usage:    "Path to the directory where the mkdocs.yml file is located",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "repo-url",
+				Usage:    "Full URL of the repository on GitHub",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "default-branch",
+				Usage:    "Name of the default branch",
+				Required: true,
+			},
+			&cli.BoolFlag{
+				Name:     "dry-run",
+				Usage:    "Print changes without modifying the files",
+				Required: false,
+				Value:    false,
+			},
+		},
+		Action: func(cliCtx *cli.Context) error {
+			repoURL := cliCtx.String("repo-url")
+			defaultBranch := cliCtx.String("default-branch")
+			rootDir := cliCtx.String("root-dir")
+
+			ctx := cliCtx.Context
+			var logger *slog.Logger
+			if os.Getenv("GITHUB_ACTIONS") == "true" {
+				handler := &human.Handler{}
+				logger = slog.New(&actionslog.Wrapper{
+					Handler: handler.WithOutput,
+				})
+			} else {
+				if term.IsTerminal(int(os.Stderr.Fd())) {
+					logger = slog.New(
+						tint.NewHandler(os.Stderr, nil),
+					)
+				} else {
+					logger = slog.New(
+						slog.NewTextHandler(os.Stderr, nil),
+					)
+				}
+			}
+			ctrl := controller{
+				filesys:       afero.NewOsFs(),
+				dryRun:        cliCtx.Bool("dry-run"),
+				logger:        logger,
+				rootDirectory: rootDir,
+				repoURL:       repoURL,
+				defaultBranch: defaultBranch,
+			}
+			return ctrl.run(ctx)
+		},
+	}
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+	app.RunContext(ctx, os.Args)
+}
+
+type controller struct {
+	dryRun        bool
+	logger        *slog.Logger
+	filesys       afero.Fs
+	defaultBranch string
+	repoURL       string
+	rootDirectory string
+
+	// Private: This will be set during the run
+	docsRootDirectory string
+}
+
+func (ctrl *controller) run(ctx context.Context) error {
+	docsRootDirectory, err := ctrl.getDocsDir(ctx, ctrl.rootDirectory)
+	ctrl.docsRootDirectory = docsRootDirectory
+	if err != nil {
+		return err
+	}
+	return afero.Walk(ctrl.filesys, docsRootDirectory, func(path string, d fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+		if !d.IsDir() && strings.HasSuffix(d.Name(), ".md") {
+			if err := ctrl.updateRelativeLinks(ctx, path); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+}
+
+func (ctrl *controller) updateRelativeLinks(ctx context.Context, path string) error {
+	logger := ctrl.logger.With(slog.String("path", path))
+	renderer := markdown.NewRenderer()
+	markdown := goldmark.New(goldmark.WithRenderer(renderer))
+	transformer := &relativeLinkASTTransformer{
+		filesys:           ctrl.filesys,
+		ctx:               ctx,
+		logger:            logger,
+		rootDirectory:     ctrl.rootDirectory,
+		docsRootDirectory: ctrl.docsRootDirectory,
+		path:              path,
+		repoURL:           ctrl.repoURL,
+		defaultBranch:     ctrl.defaultBranch,
+		dryRun:            ctrl.dryRun,
+	}
+	markdown.Parser().AddOptions(parser.WithASTTransformers(util.Prioritized(transformer, 999)))
+	source, err := afero.ReadFile(ctrl.filesys, path)
+	if err != nil {
+		return err
+	}
+	out := bytes.Buffer{}
+	if err := markdown.Convert(source, &out); err != nil {
+		return err
+	}
+	if transformer.changed {
+		if ctrl.dryRun {
+			// Print a diff only and then return
+			changes := udiff.Strings(string(source), out.String())
+			unifiedChanges, err := udiff.ToUnifiedDiff(path, path+".updated", string(source), changes, 5)
+			if err != nil {
+				return err
+			}
+			fmt.Println(unifiedChanges)
+			return nil
+		}
+		// Only update the original file if there were actual changes made:
+		if err := afero.WriteFile(ctrl.filesys, path, out.Bytes(), 0600); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (ctrl *controller) getDocsDir(ctx context.Context, root string) (string, error) {
+	cfg := mkdocsYaml{}
+	content, err := afero.ReadFile(ctrl.filesys, filepath.Join(root, "mkdocs.yml"))
+	if err != nil {
+		return "", err
+	}
+	if err := yaml.Unmarshal(content, &cfg); err != nil {
+		return "", err
+	}
+	if filepath.IsAbs(cfg.DocsDir) {
+		return cfg.DocsDir, nil
+	}
+	return filepath.Join(root, cfg.DocsDir), nil
+}
+
+type mkdocsYaml struct {
+	DocsDir string `yaml:"docs_dir"`
+}
+
+type relativeLinkASTTransformer struct {
+	ctx               context.Context
+	logger            *slog.Logger
+	filesys           afero.Fs
+	rootDirectory     string
+	docsRootDirectory string
+	path              string
+	repoURL           string
+	defaultBranch     string
+	changed           bool
+	dryRun            bool
+}
+
+func (transformer *relativeLinkASTTransformer) Transform(node *ast.Document, reader text.Reader, pc parser.Context) {
+	ast.Walk(node, func(n ast.Node, entering bool) (ast.WalkStatus, error) {
+		// We only care about links. Images etc. are not handled:
+		link, ok := n.(*ast.Link)
+		if !ok {
+			return ast.WalkContinue, nil
+		}
+		// If the destination points somewhere outside of the root
+		// directory, then we care:
+		dst := string(link.Destination)
+		if !strings.HasPrefix(dst, "..") {
+			return ast.WalkContinue, nil
+		}
+
+		absPath := filepath.Join(filepath.Dir(transformer.path), dst)
+		destStat, err := transformer.filesys.Stat(absPath)
+		if err != nil {
+			return ast.WalkStop, err
+		}
+		rel, _ := filepath.Rel(transformer.docsRootDirectory, absPath)
+		if !strings.HasPrefix(rel, "..") {
+			return ast.WalkContinue, nil
+		}
+
+		typeSegment := "tree"
+		if !destStat.IsDir() {
+			typeSegment = "blob"
+		}
+		newDest := strings.Replace(absPath, strings.TrimSuffix(transformer.rootDirectory, "/"), transformer.repoURL+"/"+typeSegment+"/"+transformer.defaultBranch, 1)
+		transformer.logger.InfoContext(transformer.ctx, "rewriting path", slog.String("old-dest", dst), slog.String("new-dest", newDest), slog.Bool("dry-run", transformer.dryRun))
+		link.Destination = []byte(newDest)
+		transformer.changed = true
+		return ast.WalkContinue, nil
+	})
+}
---
+++ actions/techdocs-rewrite-relative-links/main_test.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	"github.com/neilotoole/slogt"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUpdateRelativeLinks(t *testing.T) {
+	logger := slogt.New(t)
+	tests := map[string]struct {
+		testDirectory       map[string]string
+		testDirectorySetup  func(afero.Fs, string)
+		fileUnderTest       string
+		expectedFileContent string
+		dryRun              bool
+	}{
+		"rewrites-outside-link": {
+			testDirectorySetup: func(fs afero.Fs, rootDir string) {
+				afero.WriteFile(fs, filepath.Join(rootDir, "README.md"), []byte("outside file"), 0600)
+				afero.WriteFile(fs, filepath.Join(rootDir, "docs/README.md"), []byte("Hello world [inside](./README.md) [outside](../README.md)"), 0600)
+			},
+			fileUnderTest:       "docs/README.md",
+			expectedFileContent: "Hello world [inside](./README.md) [outside](https://github.com/grafana/dummy/blob/main/README.md)\n",
+		},
+		"ignores-external-links": {
+			testDirectorySetup: func(fs afero.Fs, rootDir string) {
+				afero.WriteFile(fs, filepath.Join(rootDir, "docs/README.md"), []byte("Hello world [external](https://example.org)"), 0600)
+			},
+			fileUnderTest:       "docs/README.md",
+			expectedFileContent: "Hello world [external](https://example.org)",
+		},
+		"dry-run-does-not-modify": {
+			testDirectorySetup: func(fs afero.Fs, rootDir string) {
+				afero.WriteFile(fs, filepath.Join(rootDir, "README.md"), []byte("outside file"), 0600)
+				afero.WriteFile(fs, filepath.Join(rootDir, "docs/README.md"), []byte("Hello world [inside](./README.md) [outside](../README.md)"), 0600)
+			},
+			fileUnderTest:       "docs/README.md",
+			expectedFileContent: "Hello world [inside](./README.md) [outside](../README.md)",
+			dryRun:              true,
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			filesys := afero.NewMemMapFs()
+			rootDir := "/root"
+			if test.testDirectorySetup != nil {
+				test.testDirectorySetup(filesys, rootDir)
+			}
+			ctrl := controller{
+				filesys:           filesys,
+				dryRun:            test.dryRun,
+				rootDirectory:     rootDir,
+				logger:            logger,
+				defaultBranch:     "main",
+				repoURL:           "https://github.com/grafana/dummy",
+				docsRootDirectory: filepath.Join(rootDir, "docs"),
+			}
+			testFilePath := filepath.Join(rootDir, test.fileUnderTest)
+			require.NoError(t, ctrl.updateRelativeLinks(context.Background(), filepath.Join(rootDir, test.fileUnderTest)))
+			updatedContent, err := afero.ReadFile(filesys, testFilePath)
+			require.NoError(t, err)
+			require.Equal(t, test.expectedFileContent, string(updatedContent))
+		})
+	}
+}
+
+func TestGetDocsRoot(t *testing.T) {
+	logger := slogt.New(t)
+	tests := map[string]struct {
+		expectError             bool
+		expectedRelativeDocsDir string
+		expectedAbsoluteDocsDir string
+		rootDirectorySetup      func(afero.Fs, string)
+	}{
+		"no-mkdocs-yml": {
+			expectError: true,
+		},
+		"mkdocs-yml-relative-docs-dir": {
+			expectError: false,
+			rootDirectorySetup: func(fs afero.Fs, rootDir string) {
+				afero.WriteFile(fs, filepath.Join(rootDir, "mkdocs.yml"), []byte(`docs_dir: "docs"`), 0600)
+			},
+			expectedRelativeDocsDir: "docs",
+		},
+		"mkdocs-yml-absolute-docs-dir": {
+			expectError: false,
+			rootDirectorySetup: func(fs afero.Fs, rootDir string) {
+				afero.WriteFile(fs, filepath.Join(rootDir, "mkdocs.yml"), []byte(`docs_dir: "/tmp/docs"`), 0600)
+			},
+			expectedAbsoluteDocsDir: "/tmp/docs",
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			rootDir := "/root"
+			filesys := afero.NewMemMapFs()
+			if test.rootDirectorySetup != nil {
+				test.rootDirectorySetup(filesys, rootDir)
+			}
+			ctrl := controller{
+				filesys:       filesys,
+				rootDirectory: rootDir,
+				logger:        logger,
+				defaultBranch: "main",
+				repoURL:       "https://github.com/grafana/dummy",
+			}
+			docsDir, err := ctrl.getDocsDir(context.Background(), rootDir)
+			if test.expectError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+			if test.expectedAbsoluteDocsDir != "" {
+				require.Equal(t, test.expectedAbsoluteDocsDir, docsDir)
+			}
+			if test.expectedRelativeDocsDir != "" {
+				require.Equal(t, filepath.Join(rootDir, test.expectedRelativeDocsDir), docsDir)
+			}
+		})
+	}
+}
+
+func createTestDirectory(t *testing.T, filesys afero.Fs, rootDir string, content map[string]string) {
+	t.Helper()
+	for filename, filecontent := range content {
+		dir := filepath.Dir(filename)
+		fullPath := filepath.Join(rootDir, dir)
+		require.NoError(t, filesys.MkdirAll(fullPath, 0700))
+		fullFilePath := filepath.Join(rootDir, filename)
+		require.NoError(t, afero.WriteFile(filesys, fullFilePath, []byte(filecontent), 0600))
+	}
+}
