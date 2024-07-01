e4a3cc54f37dddd67841b85f22f48583c4da86e0
Iain Lane <iain@grafana.com>
2024-07-01 11:08:43 +0100

    test lint

--- utils/generate-table-from-workflow-inputs/generate-table-from-inputs.py
+++ utils/generate-table-from-workflow-inputs/generate-table-from-inputs.py
@@ -7,10 +7,10 @@ Usage:
     python markdown-table-from-workflow-inputs.py <path-to-workflow-yaml-file>
 """
 
+import argparse
 import sys
 import yaml
 from pathlib import Path
-from pprint import pprint
 
 
 def get_longest_length(arr_string: list[str]) -> int:
@@ -18,34 +18,54 @@ def get_longest_length(arr_string: list[str]) -> int:
 
 
 def main(workflow_file_path: Path):
-    with open(workflow_file, "r") as f:
+    with open(workflow_file_path, "r") as f:
         workflow = yaml.safe_load(f)
 
     try:
+        print(workflow)
         inputs = workflow[True]["workflow_call"]["inputs"]
     except KeyError:
-        print("Could not find the inputs in the workflow file. Please ensure the workflow is a reusable one and has inputs.")
-        sys.exit(1)
+        # Is it an action rather than a reusable workflow?
+        try:
+            inputs = workflow["inputs"]
+        except KeyError:
+            print(
+                "Could not find the inputs in the workflow file. Please ensure the workflow is a reusable one and has inputs."
+            )
+            sys.exit(1)
 
     # adding 2 for the backticks
     name_column_padding = get_longest_length(list(inputs.keys())) + 2
-    description_column_padding = get_longest_length([value.get("description", "") for value in inputs.values()])
-    type_column_padding = get_longest_length([str(value.get("type", "")) for value in inputs.values()])
+    description_column_padding = get_longest_length(
+        [value.get("description", "") for value in inputs.values()]
+    )
+    type_column_padding = get_longest_length(
+        [str(value.get("type", "")) for value in inputs.values()]
+    )
 
-    print(f"| {'Name'.ljust(name_column_padding)} | {'Type'.ljust(type_column_padding)} | {'Description'.ljust(description_column_padding)} |")
-    print(f"| {'-' * name_column_padding} | {'-' * type_column_padding} | {'-' * description_column_padding} |")
+    print(
+        f"| {'Name':<{name_column_padding}} | {'Type':<{type_column_padding}} | {'Description':<{description_column_padding}} |"
+    )
+    print(
+        f"| {'-':->{name_column_padding}} | {'-':->{type_column_padding}} | {'-':->{description_column_padding}} |"
+    )
 
-    for name, value in inputs.items():
+    for name, value in sorted(inputs.items()):
         description = value.get("description", "")
         the_type = str(value.get("type", ""))
-        formatted_name = f'`{name}`'.ljust(name_column_padding)
+        formatted_name = f"`{name}`".ljust(name_column_padding)
         formatted_type = the_type.ljust(type_column_padding)
         formatted_description = description.ljust(description_column_padding)
         print(f"| {formatted_name} | {formatted_type} | {formatted_description} |")
 
+
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(__doc__)
-        sys.exit(1)
-    workflow_file = sys.argv[1]
-    main(Path(workflow_file))
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "workflow_file", type=Path, help="Path to the workflow YAML file"
+    )
+    args = parser.parse_args()
+
+    main(args.workflow_file)
