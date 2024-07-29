62ef14f30ce2deecfbf7e9aebd82bba4a679903b
Julien Duchesne <julien.duchesne@grafana.com>
2024-07-29 15:18:57 -0400

    Add OpenAPI client generation action
    This action will be used to generate OpenAPI clients from the various OpenAPI specs we have in the org.
    
    It will replace the copy-pasted workflows in these repos:
    
    - https://github.com/grafana/slo-openapi-client
    - https://github.com/grafana/grafana-com-public-clients
    
    And then, it can be used to expand the client generation feature and make it available to other repos.

---
+++ actions/generate-openapi-clients/generate.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+set -eo pipefail
+
+# Download openapi-generator-cli
+wget -nv "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/${OPENAPI_GENERATOR_VERSION}/openapi-generator-cli-${OPENAPI_GENERATOR_VERSION}.jar" -O ./openapi-generator-cli.jar
+trap "rm -f ./openapi-generator-cli.jar" EXIT
+
+# Generate Go client (TODO: Add support for other languages)
+GO_DIR="${OUTPUT_DIR}/go"
+rm -rf "${GO_DIR}"
+java -jar openapi-generator-cli.jar generate \
+  -i "${SPEC_PATH}" \
+  -g go \
+  -o "${GO_DIR}" \
+  --git-user-id "grafana" \
+  --git-repo-id "${REPO_NAME}/go" \
+  --package-name "${PACKAGE_NAME}" \
+  -p disallowAdditionalPropertiesIfNotPresent=false \
+  -t "${GITHUB_ACTION_PATH}/templates/go"
+
+pushd "${GO_DIR}" && go mod tidy && popd
+if ! command -v goimports &> /dev/null
+then
+    go install golang.org/x/tools/cmd/goimports@latest
+fi
+find "${GO_DIR}" -name \*.go -exec goimports -w {} \;
+
