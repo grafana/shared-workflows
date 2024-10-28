d4f2fb763b4e490abe32017284815313061b28d3
Nafisa Tabassum <nafisa.tabassum@grafana.com>
2024-10-28 13:50:43 -0600

    replace with aws ops instance

--- actions/trigger-argo-workflow/cmd/trigger-argo-workflow/argo.go
+++ actions/trigger-argo-workflow/cmd/trigger-argo-workflow/argo.go
@@ -35,7 +35,6 @@ type App struct {
 var instanceToHost = map[string]string{
 	"dev":     "argo-workflows-dev.grafana.net:443",
 	"ops":     "argo-workflows.grafana.net:443",
-	"ops-aws": "argo-workflows-aws.grafana.net:443",
 }
 
 func (a App) server() string {
