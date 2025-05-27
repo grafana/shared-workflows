2bb44ea77c9b48c7766bdee0fe6951f5eca758b7
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2025-05-27 15:10:43 +0200

    Don't mention people

--- actions/analyze-test-failures/cmd/analyze-test-failures/github.go
+++ actions/analyze-test-failures/cmd/analyze-test-failures/github.go
@@ -275,7 +275,7 @@ This test failed **{{.TotalFailures}} times** across **{{len .BranchCounts}} dif
 
 ### 🕵️ Who might know about this?
 {{- range .RecentCommits}}
-- @{{.Author}} - made a relevant commit on {{.Timestamp | formatDate}}: {{.Hash}} "{{.Title}}"
+- {{.Author}} - made a relevant commit on {{.Timestamp | formatDate}}: {{.Hash}} "{{.Title}}"
 {{- end}}
 
 👆 If any of you have a few minutes, could you take a look? You might have context on what could be causing the flakiness.
