d24aec3cf632da57a34c20e73ae3238bed1340c5
Horst Gutmann <horst.gutmann@grafana.com>
2025-06-13 19:15:57 +0200

    fix(get-latest-workflow-artifact): suggested fixes (#1026)
    ... from https://github.com/grafana/shared-workflows/pull/1022

--- actions/get-latest-workflow-artifact/src/main.ts
+++ actions/get-latest-workflow-artifact/src/main.ts
@@ -1,7 +1,18 @@
 import * as core from "@actions/core";
-import * as github from "@actions/github";
+import { getOctokit } from "@actions/github";
 import { DefaultArtifactClient } from "@actions/artifact";
 import * as filepath from "node:path";
+
+import { components } from "@octokit/openapi-types";
+
+type WorkflowRunStatus = "in_progress" | "completed";
+type Artifact = components["schemas"]["artifact"];
+type GitHubClient = ReturnType<typeof getOctokit>;
+type WorkflowRun = components["schemas"]["workflow-run"];
+type GetLatestArtifactResponse = {
+  artifact: Artifact;
+  run: WorkflowRun;
+} | null;
 
 async function main() {
   const ghToken = core.getInput("github-token", { required: true });
@@ -24,7 +35,7 @@ async function main() {
 
   path = filepath.join(path, artifactName);
 
-  const client = github.getOctokit(ghToken);
+  const client = getOctokit(ghToken);
 
   const { data } = await client.rest.pulls.get({
     owner: repoOwner,
@@ -36,7 +47,7 @@ async function main() {
 
   // Now let's get all workflows associated with this sha:
   // We start with in-progress ones if those are to be considered.
-  let found = null;
+  let found: GetLatestArtifactResponse = null;
 
   if (considerInProgress) {
     found = await getLatestArtifact(
@@ -84,45 +95,51 @@ async function main() {
 }
 
 async function getLatestArtifact(
-  client,
-  repoOwner,
-  repoName,
-  workflowId,
+  client: GitHubClient,
+  repoOwner: string,
+  repoName: string,
+  workflowId: string,
   headSha: string,
-  status: string,
+  status: WorkflowRunStatus,
   artifactName: string,
-): Promise<{ artifact: any; run: any } | null> {
-  const { data } = await client.rest.actions.listWorkflowRuns({
-    repo: repoName,
-    owner: repoOwner,
-    head_sha: headSha,
-    workflow_id: workflowId,
-    status: status,
-  });
-  if (data.workflow_runs.length === 0) {
+): Promise<GetLatestArtifactResponse> {
+  const {
+    data: { workflow_runs: workflowRuns },
+  }: { data: { workflow_runs: WorkflowRun[] } } =
+    await client.rest.actions.listWorkflowRuns({
+      repo: repoName,
+      owner: repoOwner,
+      head_sha: headSha,
+      workflow_id: workflowId,
+      status: status,
+    });
+  if (workflowRuns.length === 0) {
     console.log(`No ${status} runs found`);
     return null;
   }
-  const run = data.workflow_runs[0];
-  // Since these are pending workflows, the artifact might not be there yet. For this scenario, let's try this a couple of times:
-  for (let attempt = 0; attempt < 5; attempt++) {
-    const { data: artifacts } =
+  const run = workflowRuns[0];
+  // For in-progress workflows the artifact might not be available yet. For this scenario we do a bit of retrying:
+  const maxAttempts = status === "in_progress" ? 5 : 1;
+  for (let attempt = 1; attempt <= maxAttempts; attempt++) {
+    const {
+      data: { artifacts },
+    }: { data: { artifacts: Artifact[] } } =
       await client.rest.actions.listWorkflowRunArtifacts({
         owner: repoOwner,
         repo: repoName,
         run_id: run.id,
       });
-    const artifact = artifacts.artifacts.find(
-      (art) => art.name == artifactName,
-    );
+    const artifact = artifacts.find((art) => art.name == artifactName);
     if (artifact) {
       console.log(`Found ${status} artifact`);
       return { artifact, run };
     }
-    console.log("No artifact found, retrying in 10s");
-    await new Promise((resolve) => {
-      setTimeout(() => resolve(), 10000);
-    });
+    if (attempt < maxAttempts) {
+      console.log("No artifact found, retrying in 10s");
+      await new Promise((resolve) => setTimeout(resolve, 10_000));
+    }
   }
+  console.log("No artifact found");
+  return null;
 }
 main();
