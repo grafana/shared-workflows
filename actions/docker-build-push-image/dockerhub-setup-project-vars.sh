61045adc7e664e915ebce70392ec845278f72fdf
Ricky Whitaker <ricky.whitaker@grafana.com>
2025-09-09 20:44:15 -0500

    WIP: Restructure inputs/outputs and integrate dockerhub

--- actions/docker-build-push-image/dockerhub-setup-project-vars.sh
+++ actions/docker-build-push-image/dockerhub-setup-project-vars.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 # Required env vars (provided in workflow yaml)
-: "${DOCKERHUB_IMAGE:?IMAGE_NAME env var is required}"
+: "${DOCKERHUB_IMAGE:?DOCKERHUB_IMAGE env var is required}"
 
 echo "image=${DOCKERHUB_IMAGE}" | tee -a "${GITHUB_OUTPUT}"
